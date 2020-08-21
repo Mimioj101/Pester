aa0e87247fc70941fd503396139beb6b1577a014
vincentyang01 <vincentyangcsi@gmail.com>
2020-08-20 21:35:29 -0400

    styled

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,9 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, :age, :password, :bio, :preference, :img_url, presence: true
-    validates :name, uniqueness: true
+    #validates :name, uniqueness: true
     
     validates :password, length: {minimum: 6}
-    validates_length_of :bio, :minimum => 100
+    # validates_length_of :bio, :minimum => 100
 
-    def getCockroachesMatchedWithCurrentUser
-        arr = []
-        Match.all.each do |f|
-            if f.user_id == @current_user.id
-                arr << f.cockroach_id
-            end
-        end
-        arr1 = []
-        arr.each do |g|
-            instance = Cockroach.find_by(id: g)
-            arr1 << instance
-        end
-        arr1
-        byebug
-    end
 end
